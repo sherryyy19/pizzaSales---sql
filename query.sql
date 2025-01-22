9e791a8538712be76292c63ba60116e9cfb05042
Sharjeel Ahmed <84346930+shery47@users.noreply.github.com>
2025-01-22 09:12:29 -0700

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,20 @@
+create database pizzaonline;
+
+use pizzaonline;
+-- importing data from excel
+
+create table orders (
+order_id int not null,
+order_date date not null,
+order_time time not null,
+primary key (order_id)
+);
+
+create table order_details (
+order_details_id int not null,
+order_id int not null,
+pizza_id text not null,
+quantity integer not null,
+primary key (order_details_id)
+);
+
